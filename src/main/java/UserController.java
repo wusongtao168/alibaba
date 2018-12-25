63fa2f395b76592cf413f11a654187e2c7ab1e34
wusongtao <754743116@qq.com>
2018-12-25 17:01:11 +0800

    first commit

--- src/main/java/UserController.java
+++ src/main/java/UserController.java
@@ -4,10 +4,6 @@
  */
 public class UserController {
     public static void main(String[] args) {
-        System.out.println("hohohohohohohoho");
-        System.out.println("123456");
-        System.out.println("two");
-        System.out.println("aaaa");
-        System.out.println("one");
+        System.out.println("第一次提交");
     }
 }
