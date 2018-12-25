4017c98a20051b7c2e9d17dcd64c63e4686b43ca
wusongtao <754743116@qq.com>
2018-12-25 16:06:18 +0800

    first project

---
+++ src/main/java/UserController.java
@@ -0,0 +1,9 @@
+/**
+ * @author:Wusongtao
+ * @Date:2018/12/25上午10:32
+ */
+public class UserController {
+    public static void main(String[] args) {
+        System.out.println("hohohohohohohoho");
+    }
+}
