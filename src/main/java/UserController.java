bc04688f5d2d5284a727fc12cbe8ae147df0b143
wusongtao <754743116@qq.com>
2018-12-25 16:43:12 +0800

    add two -->master

--- src/main/java/UserController.java
+++ src/main/java/UserController.java
@@ -6,5 +6,6 @@ public class UserController {
     public static void main(String[] args) {
         System.out.println("hohohohohohohoho");
         System.out.println("123456");
+        System.out.println("two");
     }
 }
