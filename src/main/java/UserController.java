7fe639204a0b15d8cb665dcd5a36693f3ba9f2ef
wusongtao <754743116@qq.com>
2018-12-25 17:54:14 +0800

    ad

--- src/main/java/UserController.java
+++ src/main/java/UserController.java
@@ -4,8 +4,6 @@
  */
 public class UserController {
     public static void main(String[] args) {
-        System.out.println("000000");
-        System.out.println("123456");
-        System.out.println("22222");
+
     }
 }
