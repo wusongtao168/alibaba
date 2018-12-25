35ee9334bf9eb90b01943e610d17c47bd6582cae
wusongtao <754743116@qq.com>
2018-12-25 16:41:10 +0800

    add one

--- src/main/java/UserController.java
+++ src/main/java/UserController.java
@@ -7,5 +7,6 @@ public class UserController {
         System.out.println("hohohohohohohoho");
         System.out.println("123456");
         System.out.println("aaaa");
+        System.out.println("one");
     }
 }
